b60cb65b3a2351268e76e74cf98f751ee35b7054
Trindade <59809207+juliano-xd@users.noreply.github.com>
2024-07-20 10:36:48 -0300

    Update main.py

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import base58
 import secp256k1
 import threading
 import os
+from time import sleep
 
 def sha256(data: bytes) -> bytes:
     return hashlib.sha256(data).digest()
@@ -19,56 +20,89 @@ def generate_address_from_private_key(private_key_hex: str) -> str:
     checksum = sha256(sha256(prefixed_hash160))[:4]
     return base58.b58encode(prefixed_hash160 + checksum).decode('utf-8')
 
-class Right(threading.Thread):
+class Worker(threading.Thread):
     def __init__(self, num: int, progress: int, target: str):
         super().__init__()
-        self.number = num
-        self.progress = progress
+        self.number = num + progress
         self.target = target
-        self.publicKey = None
         self.quantity = 0
-        self.find = False
         self.lock = threading.Lock()
 
     def generatePublic(self, number: int) -> str:
         private_key_hex = format(number, '064x')
         return generate_address_from_private_key(private_key_hex)
 
+class Right(Worker):
     def run(self):
-        while not self.find:
-            current_number = self.number + self.progress
-            self.publicKey = self.generatePublic(current_number)
-
-            if self.publicKey == self.target:
-                with self.lock:
-                    self.find = True
-                    self.number = current_number
-                    print(f"\nFind in number: {self.number}")
-                    print(f"╚═> {self.publicKey}")
-                    print(f"╚═> {format(self.number, 'x')}")
-                    break
-
+        publickey = None
+        while not publickey == self.target:
             self.number += 1
+            publickey = self.generatePublic(self.number)
             self.quantity += 1
 
-# Função para obter o número de CPUs
-def log_cpu_info():
-    num_cpus = os.cpu_count()
-    print(f"Number of CPUs: {num_cpus}")
+        with self.lock:
+            print(f"\nFind in number: {self.number}")
+            print(f"╚═> {publickey}")
+            print(f"╚═> {format(self.number, 'x')}")
 
-# Testando o código
+class Left(Worker):
+    def run(self):
+        publickey = None
+        while not publickey == self.target:
+            self.number -= 1
+            publickey = self.generatePublic(self.number)
+            self.quantity += 1
+
+        with self.lock:
+            print(f"\nFind in number: {self.number}")
+            print(f"╚═> {publickey}")
+            print(f"╚═> {format(self.number, 'x')}")
+
+
 if __name__ == "__main__":
-    log_cpu_info()  # Log do número de CPUs
 
-    startIn = int("0000000000000000000000000000000000000000000000000000000000000001", 16)
-    endIn = int("0000000000000000000000000000000000000000000000000000000000000001", 16)
-    target = "19ZewH8Kk1PDbSNdJ97FP4EiCjTRaZMZQA"
+    progress = 0
+    startIn = 0x80000
+    endIn = 0xfffff
+    size = int(endIn - startIn)
+    target = "1HsMJxNiV7TLxmoF6uJNkydxPFDog4NQum"
+    workers = []
 
-    private_key = "000000000000000000000000000000000000000000000001a838b13505b26867"
-    address = generate_address_from_private_key(private_key)
-    print(address)  # Deve gerar o endereço Bitcoin correspondente: 19ZewH8Kk1PDbSNdJ97FP4EiCjTRaZMZQA
+    def percentage(size, percent):
+        return int(size * percent).__round__()
 
-    # Exemplo de execução do worker
-    worker = Right(startIn, 1, target)
-    worker.start()
-    worker.join()
+    workers.append(Right(startIn, progress, target))  # 0
+
+    workers.append(Left(startIn + percentage(size, 0.25), progress, target))  # 25
+    workers.append(Right(startIn + percentage(size, 0.25), progress, target))  # 25
+
+    workers.append(Left(startIn + percentage(size, 0.5), progress, target))  # 50
+    workers.append(Right(startIn + percentage(size, 0.5), progress, target))  # 50
+
+    workers.append(Left(startIn + percentage(size, 0.75), progress, target))  # 75
+    workers.append(Right(startIn + percentage(size, 0.75), progress, target))  # 75
+
+    workers.append(Left(endIn, progress, target))  # 100
+
+    print(f"Indexing total {len(workers)} workers...")
+    for worker in workers:
+        worker.start()
+    print("\nIndexing done")
+
+    def monitor():
+        seconds = 1
+        max_mps = 0
+        while any(worker.is_alive() for worker in workers):
+            mps = sum(worker.quantity for worker in workers) / seconds
+            if max_mps < mps:
+                max_mps = mps
+            print(f"\rMP/s: {mps}, Max: {max_mps}, TP: {sum(worker.quantity for worker in workers) + progress}", end='')
+            sleep(1)
+            seconds += 1
+        print("\n shutdown")
+
+    monitor_thread = threading.Thread(target=monitor)
+    monitor_thread.start()
+    monitor_thread.join()
+    for worker in workers:
+        worker.join()
